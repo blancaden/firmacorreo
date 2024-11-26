9e3c4a381eb9999e1a7c6ffce361efed4d890e91
Blanca <blankdgonzfigue@gmail.com>
2024-11-26 13:17:44 +0100

    firma de correo electrónico generada

--- firma.php
+++ firma.php
@@ -1,13 +1,94 @@
 <?php
 $title = "Firma Generada - Generador de Firmas";
-include 'header.php'; // Incluir el encabezado
+include 'header.php'; 
+
+// Capturar los datos del formulario
+$nombre = $_POST['nombre'];
+$cargo = $_POST['cargo'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$empresa = $_POST['empresa'];
+$direccion = $_POST['direccion'];
+$linkedin = $_POST['linkedin'];
+$X = $_POST['X'];
+$facebook = $_POST['facebook'];
+$youtube = $_POST['youtube'];
+$instagram = $_POST['instagram'];
+
+// generar - firma - formato HTML
+$firma_html = "
+<table style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.6;'>
+    <tr>
+        <td style='font-weight: bold; color: #007bff;'>$nombre</td>
+    </tr>
+    <tr>
+        <td style='font-style: italic; color: #555;'>$cargo</td>
+    </tr>
+    <tr>
+        <td>$empresa</td>
+    </tr>
+    <tr>
+        <td>
+            <a href='mailto:$correo' style='color: #007bff; text-decoration: none;'>$correo</a>
+        </td>
+    </tr>";
+
+if ($telefono) {
+    $firma_html .= "<tr><td>Tel: $telefono</td></tr>";
+}
+if ($direccion) {
+    $firma_html .= "<tr><td>Dirección: $direccion</td></tr>";
+}
+
+
+$firma_html .= "<tr><td style='padding-top: 10px;'>";
+
+
+if ($linkedin) {
+    $firma_html .= "<a href='$linkedin' target='_blank' style='text-decoration: none; margin-right: 10px;'> 
+                        <img src='img/linkedin.png' alt='LinkedIn' style='width: 24px; height: 24px;'> 
+                    </a>";
+}
+
+
+if ($X) {
+    $firma_html .= "<a href='$X' target='_blank' style='text-decoration: none; margin-right: 10px;'> 
+                        <img src='img/red-x.png' alt='Twitter' style='width: 24px; height: 24px;'> 
+                    </a>";
+}
+
+
+if ($facebook) {
+    $firma_html .= "<a href='$facebook' target='_blank' style='text-decoration: none;'> 
+                        <img src='img/facebook.png' alt='Facebook' style='width: 24px; height: 24px;'> 
+                    </a>";
+}
+
+
+if ($youtube) {
+    $firma_html .= "<a href='$youtube' target='_blank' style='text-decoration: none;'> 
+                        <img src='img/youtube.png' alt='Youtube' style='width: 24px; height: 24px;'> 
+                    </a>";
+}
+
+if ($instagram) {
+    $firma_html .= "<a href='$instagram' target='_blank' style='text-decoration: none;'> 
+                        <img src='img/instagram.png' alt='Instagram' style='width: 24px; height: 24px;'> 
+                    </a>";
+}
+
+
+$firma_html .= "</td></tr></table>";
 ?>
+
 <div class="container">
     <h2>Firma Generada</h2>
     <div class="firma">
-        <!-- Código de la firma generada -->
+       
+        <?php echo $firma_html; ?>
     </div>
 </div>
+
 <?php
-include 'footer.php'; // Incluir el pie de página
+include 'footer.php'; 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $title = "Inicio - Generador de Firmas";
-include 'header.php'; // Incluir el encabezado
+include 'header.php'; 
 ?>
 <div class="container">
     <form action="firma.php" method="POST">
@@ -19,12 +19,28 @@ include 'header.php'; // Incluir el encabezado
         <label for="empresa">Nombre de la Empresa</label>
         <input type="text" id="empresa" name="empresa" required>
 
-        <label for="direccion">Dirección (opcional)</label>
+        <label for="direccion">Dirección</label>
         <textarea id="direccion" name="direccion"></textarea>
+
+        <label for="linkedin">LinkedIn</label>
+        <input type="text" id="linkedin" name="linkedin">
+
+        <label for="X">X</label>
+        <input type="text" id="X" name="X">
+
+        <label for="facebook">Facebook</label>
+        <input type="text" id="facebook" name="facebook">
+
+        <label for="instagram">Instagram</label>
+        <input type="text" id="instagram" name="instagram">
+        
+        <label for="youtube">Youtube</label>
+        <input type="text" id="youtube" name="youtube">
+
 
         <button type="submit">Generar Firma</button>
     </form>
 </div>
 <?php
-include 'footer.php'; // Incluir el pie de página
+include 'footer.php'; 
 ?>
